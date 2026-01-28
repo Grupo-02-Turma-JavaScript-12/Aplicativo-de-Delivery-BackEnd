48d1f06f3650907582234056ebedeb6e412d226e
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2026-01-28 16:20:16 -0300

    atualizando erro do swagger

--- src/estabelecimento/entities/estabelecimento.entity.ts
+++ src/estabelecimento/entities/estabelecimento.entity.ts
@@ -1,7 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Pedido } from '../../pedido/entities/pedido.entity';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_estabelecimentos' })
 export class Estabelecimento {
@@ -25,6 +25,6 @@ export class Estabelecimento {
   taxa_entrega: number;
 
   @OneToMany(() => Pedido, (pedido) => pedido.estabelecimento)
-  @ApiProperty({ type: () => Pedido })
+  @ApiProperty()
   pedido: Pedido[];
 }
