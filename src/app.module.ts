0461a879f95ae9e1c477117a2d228fcaed16430d
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2026-01-28 10:31:24 -0300

    criado crud estabelecimentos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_delivery',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    EstabelecimentoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
