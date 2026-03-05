3580157d0eef8da6b87a4ded608f08b9f91c1a84
mc4rvalho <mcarvalho093@gmail.com>
2026-03-05 18:09:55 -0300

    ajustes

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -55,7 +55,7 @@ export class AuthService {
       nome: buscaUsuario.nome,
       usuario: usuarioLogin.usuario,
       senha: '',
-      foto: buscaUsuario.foto,
+      foto: buscaUsuario.fot,
       tipo: buscaUsuario.tipo,
       token: `Bearer ${this.jwtService.sign(payload)}`,
     };
