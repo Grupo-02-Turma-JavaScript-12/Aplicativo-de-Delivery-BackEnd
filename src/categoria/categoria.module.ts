b34250f7c813afbf4e54b7054ef07c3d34a2f41f
allysonaggp <allysonaggp@gmail.com>
2026-03-04 23:57:44 -0300

    fix module dependency

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EstabelecimentoModule } from '../estabelecimento/estabelecimento.module';
 import { Categoria } from './entities/categoria.entity';
 import { CategoriaService } from './services/categoria.service';
 import { CategoriaController } from './controllers/categoria.controller';
+import { ProdutoModule } from '../produtos/produto.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Categoria]), EstabelecimentoModule],
+  imports: [TypeOrmModule.forFeature([Categoria]), ProdutoModule],
   providers: [CategoriaService],
   controllers: [CategoriaController],
   exports: [CategoriaService],
