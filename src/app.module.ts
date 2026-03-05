3cef004a91c3949080462a8249a17f4710b54e4f
allysonaggp <allysonaggp@gmail.com>
2026-03-04 23:00:08 -0300

    substituindo DevService por ProdService no módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,14 @@ import { ProdService } from './data/services/prod.service';
 import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
 import { PedidoModule } from './pedido/pedido.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { DevService } from './data/services/dev.service';
+// import { DevService } from './data/services/dev.service';
 import { ProdutoModule } from './produtos/produto.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     UsuarioModule,
